3dbe7cebc09e9dbfbdfd3e4f89f3859899537adf
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-15 16:58:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method
 
 def speak_to_grandma
+  if speak_to_grandma="I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 #  if phrase =="I LOVE YOU GRANDMA!"
 #    return "I LOVE YOU TOO PUMPKIN!"
