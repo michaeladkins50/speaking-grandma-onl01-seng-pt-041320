b06763ae528ba114c852ec550a93d9d181007bf0
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-15 17:06:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,11 +4,6 @@ def speak_to_grandma
   #if phrase="I LOVE YOU GRANDMA!"
   #  return "I LOVE YOU TOO PUMPKIN!"
 end
-#  if phrase =="I LOVE YOU GRANDMA!"
-#    return "I LOVE YOU TOO PUMPKIN!"
-#end
-
-#speak_to_grandma()
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
