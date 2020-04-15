ea058d8b1fd1d68ef3176281e8c6f88ac4ee8d00
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-15 17:37:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,15 @@
 
 def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "HUH?! SPEAK UP, SONNY!"
+    return "I LOVE YOU TOO PUMPKIN!"    
+
+  elsif phrase==phrase.upcase
+    return "NO, NOT SINCE 1938!"
+
+  else return "HUH?! SPEAK UP, SONNY!"
+    
+      
   end
 end
 # Whatever you say to grandma, she should respond with
